74718f6baea9d5fc45534b7b2bf2cc46c1f5a591
danjiehu <danjiehu@outlook.com>
2022-10-26 11:11:16 +0100

    set up server side js

---
+++ MyFirstProject/public/javascripts/week7.js
@@ -0,0 +1,15 @@
+function numbers() {
+    let sHTML = "<b>";
+    sHTML += 1 + "<br>" + 2 + "<br>" + 3 + "<br>" + 4 + "<br>";
+    sHTML += "</b>";
+    clientSideContent.innerHTML = sHTML;
+}
+
+function multiply() {
+    inputNumber.value = inputNumber.value * 3;
+}
+
+let car = { make: "Ford", model: "Mondeo", age: 2 };
+
+function showVehicles() {
+}
