433f8d46becdd2a20892194769d58178248ba11e
danjiehu <danjiehu@outlook.com>
2022-11-03 17:15:45 +0000

    added javascript file

---
+++ assignment-four/public/javascripts/script.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+
+let arr = [];
+let demoComments = document.getElementsByName("demo");
+
+console.log(document.getElementsByName("demo"));
+arr = Array.from(demoComments);
+console.log(arr);
+
