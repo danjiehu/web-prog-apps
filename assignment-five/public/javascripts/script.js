d637cbbba115247af7f7d9c04f29156ef43de2d2
danjiehu <danjiehu@outlook.com>
2022-11-19 18:49:28 +0000

    removed comment.js and merged in script

--- assignment-five/public/javascripts/script.js
+++ assignment-five/public/javascripts/script.js
@@ -81,3 +81,37 @@ function refreshCount() {
     }
 }
 
+// client JS function 1: postComment()
+function postComment() {
+
+    let xhr = new XMLHttpRequest();
+    let url = "https://us-central1-assignment-five-3959f.cloudfunctions.net/storeComments";
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        let DONE = 4;
+        let OK = 200;
+        if (xhr.readyState === DONE) {
+            if (xhr.status === OK) {
+                postbtn.disabled = false;
+                handle.value = null; // clearing input for next comment
+                comment.value = null; // clearing input for next comment
+                // getComments();
+            } else {
+                console.log("error: ", xhr.status);
+
+            } // end of xhr status check
+        } // end of xhr ready state check
+    } // end of readystatechange
+
+    console.log(JSON.stringify({ "handle": handle.value, "comment": comment.value }));
+    xhr.send(JSON.stringify({ "handle": handle.value, "comment": comment.value }));
+    postbtn.disabled = true;
+
+} // end of postComment()
+
+// client JS function 2: getComments()
+
+
+// client JS functon 3: deleteComment()
